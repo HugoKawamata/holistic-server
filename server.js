20f5c12e3128f93c8b55fb9288360a1f0fe5d86c
Hugo Kawamata <hugokawamata@gmail.com>
2020-04-04 14:31:48 +1000

    🚧 Attempt to connect to postgres

--- server.js
+++ server.js
@@ -2,6 +2,18 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import { buildSchema } from 'graphql';
+import { Pool, Client } from 'pg';
+
+require('dotenv').config();
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
@@ -13,6 +25,17 @@ const schema = buildSchema(`
 // The root provides a resolver function for each API endpoint
 const root = {
   hello: () => 'Hello world!',
+  now: () => {
+    let time;
+    pool.query('SELECT NOW()', (err, res) => {
+      console.log(err, res);
+      if (res) {
+        time = res;
+      }
+      pool.end();
+    });
+    return time;
+  },
 };
 
 const app = express();
