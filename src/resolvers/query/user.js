2121d2db1856e2a46a99d7d53f88b5fecd07efcb
Hugo Kawamata <hugokawamata@gmail.com>
2020-05-03 15:36:31 +1000

    ✨ Only allow authed user to get their data

--- src/resolvers/query/user.js
+++ src/resolvers/query/user.js
@@ -1,9 +1,17 @@
 // @flow
 
 export const userResolver = (pg) => {
-  return async (obj, args, context, info) =>
-    console.log("context in userResolver", context, context.session) ||
-    (await pg("accounts")
+  return async (obj, args, context, info) => {
+    if (
+      context.session.passport.user == null ||
+      context.session.passport.email != args.email
+    ) {
+      // Only let authenticated user retrieve their own data
+      return null;
+    }
+
+    return await pg("accounts")
       .where("email", args.email)
-      .then((users) => users[0]));
+      .then((users) => users[0]);
+  };
 };
