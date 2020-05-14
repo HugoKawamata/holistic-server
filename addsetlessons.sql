c173f68e1e78dee81bb6ce4beda94b0af5000cc6
Hugo Kawamata <hugokawamata@gmail.com>
2020-05-14 16:35:09 +1000

    ✨ Add all lectures

--- addsetlessons.sql
+++ addsetlessons.sql
@@ -8,6 +8,15 @@ VALUES ('HIRAGANA_A', 'Hiragana Lesson 1', 'https://res.cloudinary.com/holistic-
  ('HIRAGANA_DA', 'Hiragana Lesson 4 - Bonus Round', 'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262008/lessons/hiragana-da/title.png'),
  ('HIRAGANA_NA', 'Hiragana Lesson 4', 'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262008/lessons/hiragana-na/title.png'),
  ('HIRAGANA_N', 'Hiragana Lesson 4 - Bonus Round', 'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262008/lessons/hiragana-n/title.png');
+INSERT INTO set_lessons (content, title, titleScreenImage)
+VALUES ('HIRAGANA_HA', 'Hiragana Lesson 6', 'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262008/lessons/hiragana-ha/title.png'),
+ ('HIRAGANA_BA', 'Hiragana Lesson 6 - Bonus Round', 'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262008/lessons/hiragana-ba/title.png'),
+ ('HIRAGANA_MA', 'Hiragana Lesson 7', 'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262008/lessons/hiragana-ma/title.png'),
+ ('HIRAGANA_WA', 'Hiragana Lesson 7 - Bonus Round', 'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262008/lessons/hiragana-wa/title.png'),
+ ('HIRAGANA_YA', 'Hiragana Lesson 8', 'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262008/lessons/hiragana-ya/title.png'),
+ ('HIRAGANA_LYA', 'Hiragana Lesson 8 - Bonus Round', 'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262008/lessons/hiragana-lya/title.png'),
+ ('HIRAGANA_RA', 'Hiragana Lesson 9', 'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262008/lessons/hiragana-ra/title.png'),
+ ('HIRAGANA_PA', 'Hiragana Lesson 9 - Bonus Round', 'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262008/lessons/hiragana-pa/title.png');
 
 INSERT INTO lectures (set_lesson_id, title, text, image, position)
 VALUES (
@@ -100,6 +109,257 @@ VALUES (
 
 INSERT INTO lectures (set_lesson_id, title, text, image, position)
 VALUES (
+  7,
+  'Ta Set + TenTens',
+  'The "ta" set is the second last set that can have TenTens added, but it''s a bit different because of "chi" and "tsu".',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-da/lecture-1.png',
+  'PRETEST'
+),
+(
+  7,
+  'Ta Set + TenTens',
+  'The T sound in the normal ones ("ta", "te" and "to") becomes a D sound.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-da/lecture-2.png',
+  'PRETEST'
+),
+(
+  7,
+  'Ta Set + TenTens',
+  '"chi" and "tsu" technically can have TenTens, but it''s quite rare to see them. (They become second versions of "ji" and "zu")',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-da/lecture-3.png',
+  'PRETEST'
+),
+(
+  7,
+  'Ta Set + TenTens',
+  'For now, you can safely assume that they never get TenTens, so there''s only 3 new characters in this lesson!',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-da/lecture-4.png',
+  'PRETEST'
+);
+
+INSERT INTO lectures (set_lesson_id, title, text, image, position)
+VALUES (
+  8,
+  'The Na Set',
+  'There aren''t any strange exceptions or new rules in the "na" set! Enjoy the lesson.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-na/lecture-1.png',
+  'PRETEST'
+);
+
+
+INSERT INTO lectures (set_lesson_id, title, text, image, position)
+VALUES (
+  9,
+  'The Na Set Bonus Round',
+  '"n" is already a voiced consonant, so letters in the "na" line cannot have a TenTen. This bonus round is different.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-n/lecture-1.png',
+  'PRETEST'
+),
+(
+  9,
+  'The Letter N',
+  'In this lesson, you''ll learn a new letter, "ん" (n). This is special letter: It''s the only Japanese letter that represents a single consonant, by itself.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-n/lecture-2.png',
+  'PRETEST'
+),
+(
+  9,
+  'The Letter N',
+  'Since it''s only one new letter, we''ll cover three new, useful words which use it.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-n/lecture-3.png',
+  'PRETEST'
+);
+
+INSERT INTO lectures (set_lesson_id, title, text, image, position)
+VALUES (
+  10,
+  'The Ha Set',
+  'The sixth hiragana set is the "ha" set. It only has one tricky character.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ha/lecture-1.png',
+  'PRETEST'
+),
+(
+  10,
+  'Fu? Hu? Fhu?',
+  'The tricky one is written as "fu", but it''s pronounced more like "fhu". It''s a combination of the H and F sounds.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ha/lecture-2.png',
+  'PRETEST'
+);
+
+
+INSERT INTO lectures (set_lesson_id, title, text, image, position)
+VALUES (
+  11,
+  'Back to TenTens',
+  'The "ha" set is the last set that can have TenTens attached. When it does, it transforms the sound from "H" to "B".',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ba/lecture-1.png',
+  'PRETEST'
+),
+(
+  11,
+  'What About Fu?',
+  'Even though "ふ" is an exception, it gets changed to "bu", just like the rest of the set.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ba/lecture-2.png',
+  'PRETEST'
+);
+
+
+INSERT INTO lectures (set_lesson_id, title, text, image, position)
+VALUES (
+  12,
+  'The Ma Set',
+  'The seventh set of hiragana is the "ma" set. Only 3 more to go!',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ma/lecture-1.png',
+  'PRETEST'
+),
+(
+  12,
+  'The Ma Set',
+  'Just like the "な" set, it doesn''t have any tricky characters. Enjoy!',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ma/lecture-2.png',
+  'PRETEST'
+);
+
+INSERT INTO lectures (set_lesson_id, title, text, image, position)
+VALUES (
+  13,
+  'The Wa "Set"',
+  'This bonus round is all about the "wa" set, although it''s not much of a set.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-wa/lecture-1.png',
+  'PRETEST'
+),
+(
+  13,
+  'The Wa Set',
+  'There are only "two" characters in the set, and one of them isn''t even used in words (it''s more like a punctuation mark).',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-wa/lecture-2.png',
+  'PRETEST'
+),
+(
+  13,
+  'The Wa Set',
+  'There''s "wa", which is the real character, and "wo", which is the weird, semi-fake character.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-wa/lecture-3.png',
+  'PRETEST'
+),
+(
+  13,
+  'The Wa Set',
+  'It doesn''t make much sense now, but once you start learning how to make Japanese sentences, "wo" will become very important!',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-wa/lecture-4.png',
+  'PRETEST'
+);
+
+INSERT INTO lectures (set_lesson_id, title, text, image, position)
+VALUES (
+  14,
+  'The Ya Set',
+  'We''re getting to the end of the hiragana sets, and they do get a bit strange from here on.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ya/lecture-1.png',
+  'PRETEST'
+),
+(
+  14,
+  'The Ya Set',
+  'This set is the "ya" set, but it looks like it''s missing two characters.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ya/lecture-2.png',
+  'PRETEST'
+),
+(
+  14,
+  'The Ya Set',
+  'Japanese doesn''t actually have the yi and ye sounds. There are only three characters in this set.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ya/lecture-3.png',
+  'PRETEST'
+);
+
+INSERT INTO lectures (set_lesson_id, title, text, image, position)
+VALUES (
+  15,
+  'Little Ya, Yu, and Yo',
+  'Just like "つ", "ya", "yu" and "yo" can also become smaller to do something different.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-lya/lecture-1.png',
+  'PRETEST'
+),
+(
+  15,
+  'Little Ya, Yu and Yo',
+  'They can go after any hiragana that ends in "i" ("shi", "chi", "hi", "ni", etc.) to modify the sound.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-lya/lecture-2.png',
+  'PRETEST'
+),
+(
+  15,
+  'Little Ya, Yu and Yo',
+  'For example, "ki" + "ya" = "kya". There are lots of combinations, so they''re not all listed here.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-lya/lecture-3.png',
+  'PRETEST'
+),
+(
+  15,
+  'Little Ya, Yu and Yo',
+  'Once you get the hang of how they combine, it will feel pretty intuitive!',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-lya/lecture-3.png',
+  'PRETEST'
+);
+
+INSERT INTO lectures (set_lesson_id, title, text, image, position)
+VALUES (
+  16,
+  'The Ra Set',
+  'This is the final set of hiragana! Well done for making it all the way here.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ra/lecture-1.png',
+  'PRETEST'
+),
+(
+  16,
+  'The Ra Set',
+  'The "ra" set is hard for English speakers to pronounce, in the same way the English "l" and "r" are hard for Japanese people',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ra/lecture-2.png',
+  'PRETEST'
+),
+(
+  16,
+  'The Ra Set',
+  'It''s definitely worth checking out some videos to practice your pronunciation.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ra/lecture-3.png',
+  'PRETEST'
+),
+(
+  16,
+  'The Ra Set',
+  'Other than that, this set is pretty normal. Just one more bonus lesson after this!',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ra/lecture-4.png',
+  'PRETEST'
+);
+
+
+INSERT INTO lectures (set_lesson_id, title, text, image, position)
+VALUES (
+  17,
+  'Return To The Ha Set',
+  'The "は" set has one last trick up its sleeve, besides the TenTen.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-pa/lecture-1.png',
+  'PRETEST'
+),
+(
+  17,
+  'The Maru',
+  'The Maru is sort of like the TenTen - it changes how hiragana is pronounced, but it only works with the "は" set.',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-pa/lecture-2.png',
+  'PRETEST'
+),
+(
+  17,
+  'The Maru',
+  'When a hiragana from the "は" set has a Maru, its consonant sound changes from "H" to "P".',
+  'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-pa/lecture-3.png',
+  'PRETEST'
+);
+
+
+INSERT INTO lectures (set_lesson_id, title, text, image, position)
+VALUES (
   6,
   'The Ta Set',
   'The fourth set of hiragana is the "ta" set, which is the trickiest set of all.',
@@ -127,7 +387,6 @@ VALUES (
   'https://res.cloudinary.com/holistic-japanese/image/upload/v1587262011/lessons/hiragana-ta/lecture-4.png',
   'PRETEST'
 );
-
 
 
 INSERT INTO lectures (set_lesson_content, dialogue, image, position)
