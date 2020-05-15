fd42e1e99df79ab1e3b0407d28702124112eae54
Hugo Kawamata <hugokawamata@gmail.com>
2020-05-15 17:45:30 +1000

    ✨ Add fix mutator query args

--- modify.sql
+++ modify.sql
@@ -1,1 +1,3 @@
 UPDATE lectures SET unlocks_ids = 'HIRAGANA_KA' where set_lesson_id = 'HIRAGANA_A';
+
+update words set lesson_id = 'HIRAGANA_KA' where id >= 5 and id < 9;
--- src/resolvers/mutation/addLessonResults.js
+++ src/resolvers/mutation/addLessonResults.js
@@ -99,7 +99,7 @@ export const insertOrUpdateUserWordOrCharacter = (
 };
 
 export const addLessonResultsResolver = (pg) => {
-  return async (_, { results, userId, set_lesson_id }) => {
+  return async (_, { results, userId, setLessonId }) => {
     const wordResults = results.filter((res) => res.objectType === "WORD");
     const characterResults = results.filter(
       (res) => res.objectType === "CHARACTER"
@@ -149,14 +149,14 @@ export const addLessonResultsResolver = (pg) => {
         .then(async () => {
           // Complete current lesson
           await pg("user_set_lessons")
-            .where({ user_id: userId, set_lesson_id })
+            .where({ user_id: userId, set_lesson_id: setLessonId })
             .update({ status: "COMPLETE" })
             .transacting(trx);
 
           // Line up next lesson
           const lesson = await pg("set_lessons")
             .where({
-              id: set_lesson_id,
+              id: setLessonId,
             })
             .transacting(trx)
             .then((lessons) => lessons[0]);
