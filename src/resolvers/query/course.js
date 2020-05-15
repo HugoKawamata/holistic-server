190414a55d9b3e0d553e5058d0780fea7aa377d5
Hugo Kawamata <hugokawamata@gmail.com>
2020-05-15 16:09:03 +1000

    ✨ join courses and usercourses

--- src/resolvers/query/course.js
+++ src/resolvers/query/course.js
@@ -2,10 +2,12 @@ import { availableLessonsResolver } from "./lesson";
 
 export const availableCoursesResolver = (pg) => {
   return async (user, args) => {
-    const courses = await pg("user_courses").where({
-      user_id: user.id,
-      status: "AVAILABLE",
-    });
+    const courses = await pg("user_courses")
+      .where({
+        user_id: user.id,
+        status: "AVAILABLE",
+      })
+      .join("courses", "courses.id", "=", "user_courses.course_id");
 
     console.log("all courses got", courses);
 
