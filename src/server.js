57cfd73f4b1ec3a2e51a06f015fad9b621d60245
Hugo Kawamata <hugokawamata@gmail.com>
2020-05-24 17:11:50 +1000

    ✨ rename v5

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { ApolloServer } from "apollo-server-express";
 import session from "express-session";
 import passport from "passport";
 import GoogleTokenStrategy from "passport-google-id-token";
-import { v5 } from "uuid";
+import { v5 as uuidv5 } from "uuid";
 import knex from "knex";
 import {
   userResolver,
@@ -39,7 +39,7 @@ passport.use(
         pg.transaction(async (trx) => {
           const insert = pg("accounts")
             .insert({
-              id: v5(parsedToken.payload.email, process.env.NAMESPACE_UUID),
+              id: uuidv5(parsedToken.payload.email, process.env.NAMESPACE_UUID),
               email: parsedToken.payload.email,
               name: parsedToken.payload.name,
               picture: parsedToken.payload.picture,
