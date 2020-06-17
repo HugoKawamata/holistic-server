a8743c1570588280be1b182debb3032a9011c4cf
Hugo Kawamata <hugokawamata@gmail.com>
2020-06-17 21:09:22 +1000

    🛁 Add record of proprietary course data

---
+++ hiragana-courses.sql
@@ -0,0 +1,5 @@
+    id     |          title          |    unlocks_ids
+-----------+-------------------------+--------------------
+ HIRAGANA  | Intro to Japanese       | KATAKANA,GRAMMAR_1
+ KATAKANA  | Intro to Katakana       |
+ GRAMMAR_1 | Very Basic Conversation |
