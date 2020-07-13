c7c7f6b3667a52ea952e10c5abe11d3831a32a80
Hugo Kawamata <hugokawamata@gmail.com>
2020-07-13 15:38:14 +1000

    ✨ Change return type of sendGender

--- src/resolvers/mutation/sendGender.js
+++ src/resolvers/mutation/sendGender.js
@@ -1,5 +1,4 @@
 /* @flow */
-import { userMarshaller } from "../query/user";
 
 export const sendGenderResolver = (
   pg: any // eslint-disable-line flowtype/no-weak-types
@@ -8,8 +7,7 @@ export const sendGenderResolver = (
     _: mixed,
     { userEmail, gender }: { userEmail: string, gender: string }
   ) => {
-    return userMarshaller(
-      await pg("accounts").where({ email: userEmail }).update({ gender }, "*")
-    );
+    await pg("accounts").where({ email: userEmail }).update({ gender });
+    return true;
   };
 };
